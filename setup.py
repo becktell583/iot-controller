8edefd3a40fff73f3823715b22f522803e2948f5
unknown <becktebd@mail.uc.edu>
2022-01-24 22:11:50 -0500

    starting framework design

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='IOT Controller',
+      version='1.0',
+      description='Project to connect devices',
+      author='Brad Becktell',
+      author_email='',
+      url='https://github.com/becktell583/iot-controller',
+      packages=[''],
+     )
